ea553e9ae0685e0802669272c892173fd9a59706
Park Jinsoo <jdisis75@gmail.com>
2024-08-21 14:22:06 +0900

    test

---
+++ src/Component/IntroduceInfo/Introduce1/Introduce1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Introduce1() {
+  return (
+      <div>
+        <h1>안녕하세요 상세페이지1 입니다.</h1>
+        <p>TEST1</p>
+      </div>
+  );
+}
+
+export default Introduce1;
